64dfccc347ee03f8046d963683477ff5ac4dc35e
Arnav Kakani <144966890+ArnavKakani@users.noreply.github.com>
2025-02-04 14:42:44 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def rot13(text):
+    result = []
+    for char in text:
+        if 'a' <= char <= 'z':
+            result.append(chr((ord(char) - ord('a') + 13) % 26 + ord('a')))
+        elif 'A' <= char <= 'Z':
+            result.append(chr((ord(char) - ord('A') + 13) % 26 + ord('A')))
+        else:
+            result.append(char)
+    return ''.join(result)
+
+text = "Baez Knows!"
+encrypted_text = rot13(text)
+print(encrypted_text)
